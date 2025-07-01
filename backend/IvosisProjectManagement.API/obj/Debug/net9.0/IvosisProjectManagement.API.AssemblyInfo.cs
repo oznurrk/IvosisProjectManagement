ecb5097d37b678678d9e74e06783124579e029db
Öznur <oznurkocacik@gmail.com>
2025-07-01 11:00:25 +0300

    DTO yapısı ve controller/service güncellemeleri yapıldı.

--- backend/IvosisProjectManagement.API/obj/Debug/net9.0/IvosisProjectManagement.API.AssemblyInfo.cs
+++ backend/IvosisProjectManagement.API/obj/Debug/net9.0/IvosisProjectManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IvosisProjectManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+568b9b443348eb91fc62aa4ddfad472d9634e669")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c38289bf0553d7c9a4959a6a134a2e1de333a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("IvosisProjectManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IvosisProjectManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
