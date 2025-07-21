7158c35b02c8332f88f2f35984a337ec90c2eb9a
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2025-07-21 07:53:42 +0300

    Dashboard path eklendi

--- frontend/src/Routes/AppRoutes.jsx
+++ frontend/src/Routes/AppRoutes.jsx
@@ -10,6 +10,7 @@ import TaskChat from "../components/TaskChat/TaskChat";
 import Processes from "../pages/Processes";
 import ProcessAdd from "../components/Process/ProcessAdd";
 import TaskAdd from "../components/Tasks/TaskAdd";
+import Dashboard from "../pages/Dashboard";
 
 
 const AppRoutes = () => {
@@ -28,6 +29,7 @@ const AppRoutes = () => {
                     <Route path="/processes" element={<Processes />} />
                     <Route path="/add-process" element={<ProcessAdd />} />
                     <Route path="/add-task" element={<TaskAdd />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
                 </Route>
             </Routes>
         </BrowserRouter>
