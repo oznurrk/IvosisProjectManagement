0157e7b94d713c2b503a91bc9bbd9f97eadfc329
Öznur <oznurkocacik@gmail.com>
2025-07-29 08:16:15 +0300

    Chat post ve delete endpointleri eklendi.

--- backend/IvosisProjectManagement.API/Services/IChatService.cs
+++ backend/IvosisProjectManagement.API/Services/IChatService.cs
@@ -1,4 +1,8 @@
 public interface IChatService
 {
     Task<List<ChatMessage>> GetTaskMessagesAsync(int taskId);
+    Task<ChatMessage> SaveMessageAsync(int taskId, int userId, string message);
+    Task<ChatMessage> UpdateMessageAsync(int messageId, string newMessage);
+    Task<bool> DeleteMessageAsync(int messageId);
+
 }
