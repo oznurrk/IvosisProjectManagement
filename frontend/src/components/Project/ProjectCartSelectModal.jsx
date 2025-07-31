e1db172c099835a83a9bf2a6600f8e378be3a76f
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2025-07-31 12:56:32 +0300

    Projects sayfasındaki adres bilgileri düzenlendi, ProjectUpdateModal oluşturuldu

--- frontend/src/components/Project/ProjectCartSelectModal.jsx
+++ frontend/src/components/Project/ProjectCartSelectModal.jsx
@@ -1,15 +1,20 @@
 import { Modal, Button, Stack } from "@mantine/core";
 
-const ProjectCartSelectModal = ({ opened, onClose, projectId, onShowDetails }) => {
+const ProjectCartSelectModal = ({ opened, onClose, projectId, onShowDetails, onShowUpdate }) => {
   const goToTasks = () => {
   localStorage.setItem("selectedProjectId", projectId); // projectId'yi kaydet
   window.location.href = "/projectTasks";
 };
 
+  
+
   return (
     <Modal opened={opened} onClose={onClose} title="Proje İşlemleri" centered>
       <Stack>
-        <Button fullWidth onClick={goToTasks} color="blue">
+        <Button fullWidth onClick={onShowUpdate} color="blue">
+          Projeyi Güncelle
+        </Button>
+        <Button fullWidth onClick={goToTasks} color="gray">
           Görevleri Görüntüle
         </Button>
         <Button fullWidth onClick={onShowDetails} color="green">
